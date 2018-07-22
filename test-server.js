be0c64067fa1c4f03427d4ffab1137990c530b69
David Clark <dlc.clark@gmail.com>
2018-07-22 15:35:49 -0700

    added tests for recipes, modularized tests

--- test-server.js
+++ test-server.js
@@ -13,6 +13,90 @@ const expect = chai.expect;
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
+describe("Recipes", function(){
+  before(function(){
+    return runServer();
+  })
+
+  after(function(){
+    return closeServer();
+  })
+
+  it('should show recipe items on GET', function(){
+    return chai
+      .request(app)
+      .get('/recipes')
+      .then(function(res){
+        expect(res).to.have.status(200)
+        expect(res).to.be.json
+        expect(res.body).to.be.a('array')
+        expect(res.body.length).to.be.at.least(1)
+
+        const expectedKeys = ['name', 'id', 'ingredients']
+        res.body.forEach(function(item){
+          expect(item).to.be.a('object')
+          expect(item).to.include.keys(expectedKeys)
+        })
+      })
+  })
+  it('should add an item on POST', function(){
+    const newItem = {name: "tofu tacos", ingredients: ["tofu", "tortillas", "avocado"]}
+    return chai
+      .request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res){
+        expect(res).to.have.status(201)
+        expect(res).to.be.json
+        expect(res.body).to.be.a('object')
+        expect(res.body).to.include.keys('name', 'id', 'ingredients')
+        expect(res.body).to.deep.equal(
+          Object.assign(newItem, {id: res.body.id})
+        )
+      })
+  })
+  it('should update recipe items on PUT', function(){
+    const updateData = {
+      name: 'bar',
+      ingredients: ['buzz', 'bop']
+    }
+
+    return (
+      chai
+        .request(app)
+        .get('/recipes')
+        .then(function(res){
+          updateData.id = res.body[0].id
+          return chai
+            .request(app)
+            .put(`/recipes/${updateData.id}`)
+            .send(updateData)
+        })
+        .then(function(res){
+          expect(res).to.have.status(204)
+          // expect(res).to.be.json
+          // expect(res.body).to.be.a('object')
+          // expect(res.body).to.be.deep.equal(updateData)
+        })
+    )
+  })
+  it('should delete item on DELETE', function(){
+    return (
+      chai
+        .request(app)
+        .get('/recipes')
+        .then(function(res){
+          return chai.request(app).delete(`/recipes/${res.body[0].id}`)
+        })
+        .then(function(res){
+          expect(res).to.have.status(204)
+        })
+    )
+  })
+})
+
+
+//Shopping List
 describe("Shopping List", function() {
   // Before our tests run, we activate the server. Our `runServer`
   // function returns a promise, and we return the that promise by
